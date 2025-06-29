98308a3000785c10002f54a3752e2d1cac01014c
Ramit Dan <dan.ramit1998@gmail.com>
2025-06-29 23:33:40 +0530

    Fix the NavBar and display of game details

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,17 +1,20 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 import useGameQueryStore from "../store/gameQueryStore";
 
 const SearchInput = () => {
   const searchInputRef = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         if (searchInputRef.current) {
           setSearchText(searchInputRef.current.value);
+          navigate("/"); // Navigate to the home page after search
           // searchInputRef.current.value = ""; // Clear the input after search
         }
       }}
