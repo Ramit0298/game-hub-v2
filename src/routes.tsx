5962828264fedb998f437cdda4adb87365dd4f97
Ramit Dan <dan.ramit1998@gmail.com>
2025-06-29 21:46:58 +0530

    Handle routing errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,14 @@ import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import React from "react";
 import GameDetailPage from "./pages/GameDetailPage";
+import ErrorPage from "./pages/ErrorPage";
 const GameDetailPageLazy = React.lazy(() => import("./pages/GameDetailPage"));
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
